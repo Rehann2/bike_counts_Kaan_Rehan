b0d080f8f800da8753aeecbd59e7ee4d68a55d65
Kaan Caylan <kaancaylan1999@gmail.com>
2022-11-26 17:00:15 +0100

    Pushing the final data and estimator in CatBoostRerun

--- submissions/CatBoostRerun/estimator.py
+++ submissions/CatBoostRerun/estimator.py
@@ -45,7 +45,7 @@ def get_estimator():
     categorical_encoder = OneHotEncoder(handle_unknown="ignore")
     categorical_cols = ["counter_name", "site_name", "wind_dir"]
     numerical_cols = ['site_id', 'latitude', 'longitude', 'Temperature (C)', 'wind_speed',
-                      'Humidity', 'Visibility', 'pressure1', "Precipitation", 'sunshine_time', 'suntime', 'new_cases']
+                      'Humidity', 'Visibility', 'pressure1', 'sunshine_time', 'suntime', 'new_cases']
 
     preprocessor = ColumnTransformer(
         [
@@ -54,8 +54,8 @@ def get_estimator():
             ("scaler", scaler, numerical_cols)
         ]
     )
-    params = {'learning_rate': 0.2, 'max_depth': 13, 'l2_leaf_reg': 10,
-              'iterations': 900, 'max_bin': 285, 'task_type': 'GPU'}
+    params = {'learning_rate': 0.14, 'max_depth': 10, 'l2_leaf_reg': 10,
+              'iterations': 700, 'max_bin': 250}
 
     Boost = CatBoostRegressor(**params)
 
