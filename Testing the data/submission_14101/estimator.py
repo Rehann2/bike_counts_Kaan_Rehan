4811aea7d5925dace0b9382569fe95fdb3bd06c4
Kaan Caylan <kaancaylan1999@gmail.com>
2022-11-29 13:38:30 +0100

    Added the new changes

---
+++ Testing the data/submission_14101/estimator.py
@@ -0,0 +1,64 @@
+from catboost import CatBoostRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import FunctionTransformer
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+
+def _encode_dates(X):
+    X = X.copy()  # modify a copy of X
+    # Encode the date information from the DateOfDeparture columns
+    X.loc[:, "year"] = X["date"].dt.year
+    X.loc[:, "month"] = X["date"].dt.month
+    X.loc[:, "day"] = X["date"].dt.day
+    X.loc[:, "weekday"] = X["date"].dt.weekday
+    X.loc[:, "hour"] = X["date"].dt.hour
+
+    # Finally we can drop the original columns from the dataframe
+    return X.drop(columns=["date"])
+
+
+def _merge_external_data(X):
+    file_path = Path(__file__).parent / "external_data.csv"
+    df_ext = pd.read_csv(file_path, parse_dates=["date"])
+    X = X.copy()
+    # When using merge_asof left frame need to be sorted
+    X["orig_index"] = np.arange(X.shape[0])
+    X = pd.merge_asof(
+        X.sort_values("date"), df_ext.sort_values("date"), on="date"
+    )
+    # Sort back to the original order
+    X = X.sort_values("orig_index")
+    del X["orig_index"]
+    return X
+
+
+def get_estimator():
+    date_encoder = FunctionTransformer(_encode_dates)
+    date_cols = ["year", "month", "day", "weekday", "hour"]
+    scaler = StandardScaler()
+
+    categorical_encoder = OneHotEncoder(handle_unknown="ignore")
+    categorical_cols = ["counter_name", "site_name", "wind_dir"]
+    numerical_cols = ['site_id', 'latitude', 'longitude', 'Temperature (C)', 'wind_speed',
+                      'Humidity', 'Visibility', 'pressure1', "Precipitation", 'sunshine_time', 'suntime', 'new_cases']
+
+    preprocessor = ColumnTransformer(
+        [
+            ("date", OneHotEncoder(handle_unknown="ignore"), date_cols),
+            ("cat", categorical_encoder, categorical_cols),
+            ("scaler", scaler, numerical_cols)
+        ]
+    )
+    params = {'learning_rate': 0.1, 'max_depth': 12, 'iterations': 500}
+
+    Boost = CatBoostRegressor(**params)
+
+    pipe = make_pipeline(FunctionTransformer(
+        _merge_external_data, validate=False), date_encoder, preprocessor, Boost)
+
+    return pipe
